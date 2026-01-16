99f41be07505ad12df4d1cc601799c8e76cc2bfc
Victor Wesley <victor.infowb@gmail.com>
2026-01-16 11:39:23 -0300

    feat: Implemeta lógica para autenticação de login

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UsuarioRequest } from '../models/usuario-request.model';
+import { LoginResponse } from '../models/login-response.model';
+import { LoginRequest } from '../models/login-request.model';
 
 @Injectable({
   providedIn: 'root',
@@ -15,5 +17,10 @@ export class AuthService {
   cadastrar(dados: UsuarioRequest): Observable<any> {
     // O .post manda os dados para a URL
     return this.http.post(this.API_URL, dados);
+  }
+
+  login(dados: LoginRequest): Observable<LoginResponse> {
+    // Note que adicionamos <LoginResponse> aqui
+    return this.http.post<LoginResponse>(`${this.API_URL}/login`, dados);
   }
 }
--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from '../../../../core/services/auth.service';
+import { LoginRequest } from '../../../../core/models/login-request.model';
 
 @Component({
   selector: 'app-login',
   standalone: false,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginData: LoginRequest = {
+    email: '',
+    senha: '',
+  };
 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
+
+  onSubmit() {
+    this.authService.login(this.loginData).subscribe({
+      next: (response) => {
+        localStorage.setItem('access_token', response.token);
+
+        if (response.nome) {
+          localStorage.setItem('user_nome', response.nome);
+        }
+
+        this.snackBar.open('Login realizado com sucesso!', 'Fechar', {
+          duration: 3000,
+        });
+        this.router.navigate(['/sistema/dashboard']);
+      },
+      error: (erro) => {
+        console.error(erro);
+        this.snackBar.open('Email ou senha inválidos!', 'Fechar', {
+          duration: 3000,
+        });
+      },
+    });
+  }
 }
