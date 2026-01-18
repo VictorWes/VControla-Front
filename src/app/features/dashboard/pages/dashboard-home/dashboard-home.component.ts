904c28f3b0cb78498bd10080c0e24c17dc5d5bbe
Victor Wesley <victor.infowb@gmail.com>
2026-01-18 12:07:59 -0300

    feat:Adiciona horario dinamico, para dar bom dia, boa tarde e boa noite

--- src/app/features/dashboard/pages/dashboard-home/dashboard-home.component.ts
+++ src/app/features/dashboard/pages/dashboard-home/dashboard-home.component.ts
@@ -9,9 +9,23 @@ import { OnInit } from '@angular/core';
 })
 export class DashboardHomeComponent implements OnInit {
   nomeUsuario: string = '';
+  saudacao: string = '';
 
   ngOnInit(): void {
     const nomeCompleto = localStorage.getItem('user_nome') || 'Usuário';
     this.nomeUsuario = nomeCompleto.split(' ')[0];
+    this.definirSaudacao();
+  }
+
+  definirSaudacao() {
+    const hora = new Date().getHours();
+
+    if (hora >= 0 && hora < 12) {
+      this.saudacao = 'Bom dia';
+    } else if (hora >= 12 && hora < 18) {
+      this.saudacao = 'Boa tarde';
+    } else {
+      this.saudacao = 'Boa noite';
+    }
   }
 }
