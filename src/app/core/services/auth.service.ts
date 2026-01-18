22c355163aa848dbb83ba76a3a0a0f3622603d0b
Victor Wesley <victor.infowb@gmail.com>
2026-01-18 12:04:03 -0300

    feat:Adiciona o Bom Dia, dinamicamente de acordo com o nome do usuario

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { UsuarioRequest } from '../models/usuario-request.model';
 import { LoginResponse } from '../models/login-response.model';
 import { LoginRequest } from '../models/login-request.model';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -13,14 +14,19 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
 
-  // Método para cadastrar
   cadastrar(dados: UsuarioRequest): Observable<any> {
-    // O .post manda os dados para a URL
     return this.http.post(this.API_URL, dados);
   }
 
   login(dados: LoginRequest): Observable<LoginResponse> {
-    // Note que adicionamos <LoginResponse> aqui
-    return this.http.post<LoginResponse>(`${this.API_URL}/login`, dados);
+    return this.http.post<LoginResponse>(`${this.API_URL}/login`, dados).pipe(
+      tap((response) => {
+        localStorage.setItem('access_token', response.token);
+
+        if (response.nome) {
+          localStorage.setItem('user_nome', response.nome);
+        }
+      }),
+    );
   }
 }
--- src/app/features/dashboard/pages/dashboard-home/dashboard-home.component.ts
+++ src/app/features/dashboard/pages/dashboard-home/dashboard-home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-home',
   standalone: false,
   templateUrl: './dashboard-home.component.html',
-  styleUrl: './dashboard-home.component.scss'
+  styleUrl: './dashboard-home.component.scss',
 })
-export class DashboardHomeComponent {
+export class DashboardHomeComponent implements OnInit {
+  nomeUsuario: string = '';
 
+  ngOnInit(): void {
+    const nomeCompleto = localStorage.getItem('user_nome') || 'Usuário';
+    this.nomeUsuario = nomeCompleto.split(' ')[0];
+  }
 }
