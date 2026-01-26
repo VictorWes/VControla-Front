6992775db7b91f692a3053c574b36863f15b8d86
Victor Wesley <victor.infowb@gmail.com>
2026-01-26 11:02:31 -0300

    feat: implementa logout automatico com interceptor

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -4,27 +4,44 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
+  HttpErrorResponse,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private router: Router) {}
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler,
   ): Observable<HttpEvent<unknown>> {
     const token = localStorage.getItem('access_token');
+    let authRequest = request;
 
     if (token) {
-      const authRequest = request.clone({
+      authRequest = request.clone({
         headers: request.headers.set('Authorization', `Bearer ${token}`),
       });
-
-      return next.handle(authRequest);
     }
 
-    return next.handle(request);
+    return next.handle(authRequest).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 403 || error.status === 401) {
+          console.warn(
+            'Token expirado ou inválido (Interceptor). Fazendo logout...',
+          );
+
+          localStorage.removeItem('access_token');
+          localStorage.removeItem('user_nome');
+
+          this.router.navigate(['/auth/login']);
+        }
+
+        return throwError(() => error);
+      }),
+    );
   }
 }
