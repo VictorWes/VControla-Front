9794452865e0c04692e244f61347c20a955015a7
Victor Wesley <victor.infowb@gmail.com>
2026-01-27 11:00:46 -0300

    refactor(contas): Remove a obrigatoriedade de colocar um tipo de carteira ao cadastar uma conta

--- src/app/features/contas/components/conta-dialog/conta-dialog.component.ts
+++ src/app/features/contas/components/conta-dialog/conta-dialog.component.ts
@@ -1,8 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Conta } from '../../../../core/models/conta.model';
-import { TipoContaService } from '../../../../core/services/tipo-conta.service';
-import { TipoConta } from '../../../../core/models/tipo-conta.model';
 
 @Component({
   selector: 'app-conta-dialog',
@@ -11,7 +9,6 @@ import { TipoConta } from '../../../../core/models/tipo-conta.model';
   styleUrl: './conta-dialog.component.scss',
 })
 export class ContaDialogComponent {
-  tiposDeConta: TipoConta[] = [];
   isEdicao = false;
 
   novaConta: any = {
@@ -23,26 +20,15 @@ export class ContaDialogComponent {
   constructor(
     public dialogRef: MatDialogRef<ContaDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Conta,
-    private tipoContaService: TipoContaService,
   ) {
     if (data) {
       this.isEdicao = true;
+
       this.novaConta = { ...data };
     }
   }
 
-  ngOnInit(): void {
-    this.tipoContaService.listar().subscribe({
-      next: (lista) => {
-        this.tiposDeConta = lista;
-
-        if (!this.isEdicao && lista.length > 0) {
-          this.novaConta.tipo = lista[0];
-        }
-      },
-      error: (err) => console.error('Erro ao buscar tipos', err),
-    });
-  }
+  ngOnInit(): void {}
 
   cancelar(): void {
     this.dialogRef.close();
@@ -52,14 +38,10 @@ export class ContaDialogComponent {
     const contaParaSalvar = {
       nome: this.novaConta.nome,
       saldo: this.novaConta.saldo,
-  
+
       tipoId: this.novaConta.tipo?.id,
     };
 
     this.dialogRef.close(contaParaSalvar);
-  }
-
-  compararTipos(o1: any, o2: any): boolean {
-    return o1 && o2 ? o1.id === o2.id : o1 === o2;
   }
 }
